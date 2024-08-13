f17f84882c0a988d558a4068585c40dba3f30d2f
Niccolò Cantù <niccolo.cantu@nina.no>
2024-08-13 18:55:56 +0200

    publish only on nina branch

--- scripts/check-on-master.sh
+++ scripts/check-on-master.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Check to make sure we are on the master branch
+# Check to make sure we are on the nina branch
 # See: https://stackoverflow.com/a/34658774/3006989
 current_branch=$(git branch | grep '*');
 
-if [ "$current_branch" != "* (HEAD detached at origin/master)" -a "$current_branch" != "* master" ]; then
-    echo "Not on master - cannot proceed, please change to master by using:\n\n     git checkout master\n"
+if [ "$current_branch" != "* (HEAD detached at origin/nina)" -a "$current_branch" != "* nina" ]; then
+    echo "Not on nina - cannot proceed, please change to nina by using:\n\n     git checkout nina\n"
     exit 1
 fi
